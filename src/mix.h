23b03ba864cf6458fe71ef270447449ea6944414
Jean-François Geyelin <jfgeyelin@gmail.com>
2017-12-31 01:54:52 +0100

    Added mix with threshold function.

--- src/mix.h
+++ src/mix.h
@@ -9,6 +9,14 @@ T Mix(T const& a, T const& b, S progress) {
   return a + diff;
 }
 
+template <class T, class S>
+T Mix(T const& a, T const& b, S progress, T const& thresh_hold) {
+  T diff = (b - a);
+  if (diff > thresh_hold || diff < -thresh_hold)
+    return b;
+  return a + (diff * progress);
+}
+
 }  // namespace dux
 
 #endif  // DUX_BASE_SRC_MIX_H_

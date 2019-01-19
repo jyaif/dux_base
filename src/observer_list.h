8775fbb36474755e5ce58ffebf002959f7424a18
Jean-François Geyelin <jfgeyelin@gmail.com>
2019-01-18 17:54:44 -0800

    Update ObserverList.

--- src/observer_list.h
+++ src/observer_list.h
@@ -3,9 +3,18 @@
 
 #include <cassert>
 #include <mutex>
+#include <set>
 #include <vector>
 
+#include "thread_checker.h"
+
 namespace dux {
+
+template <class ObserverType>
+class ObserverLink {
+  ObserverType* observer_ = nullptr;
+  ObserverLink<ObserverType>* next_ = nullptr;
+};
 
 template <class ObserverType>
 // Class is not thread safe.
@@ -14,31 +23,85 @@ class ObserverList {
  public:
   // Add an observer to the list.  An observer should not be added to
   // the same list more than once.
-  void AddObserver(ObserverType* observer) { observers_.push_back(observer); }
+  void AddObserver(ObserverType* observer) {
+    VerifyThread();
+    new_observers_.push_back(observer);
+  }
 
   // Remove an observer from the list if it is in the list.
   void RemoveObserver(ObserverType* observer) {
+    VerifyThread();
     for (ObserverType*& tempObserver : observers_) {
       if (observer == tempObserver) {
-        tempObserver = observers_.back();
-        observers_.pop_back();
+        tempObserver = nullptr;
+        deletion_count_++;
         return;
       }
     }
     assert(false);
   }
 
-  bool IsEmpty() { return observers_.empty(); }
+#ifndef NDEBUG
+  // For tests only.
+  int ObserverCount() {
+    int count = 0;
+    for (auto const& observer : observers_) {
+      if (observer != nullptr) {
+        count++;
+      }
+    }
+    return count + new_observers_.size();
+  }
+#endif
+
+  void PrepareObserverListForIteration() {
+    if (deletion_count_ > 0) {
+      for (ObserverType*& tempObserver : observers_) {
+        if (tempObserver == nullptr) {
+          tempObserver = observers_.back();
+          observers_.pop_back();
+          deletion_count_--;
+          if (deletion_count_ == 0) {
+            break;
+          }
+        }
+      }
+    }
+    if (!new_observers_.empty()) {
+      observers_.insert(std::end(observers_), std::begin(new_observers_),
+                        std::end(new_observers_));
+      new_observers_.clear();
+    }
+  }
+
+  void VerifyThread() {
+#ifndef NDEBUG
+    if (!thread_checker_) {
+      thread_checker_ = std::make_unique<ThreadChecker>();
+    } else {
+      assert(thread_checker_->IsCreationThreadCurrent());
+    }
+#endif
+  }
 
   // A vector is used to keep the order of the observers deterministic.
   // Do not change to std::set.
   std::vector<ObserverType*> observers_;
+  std::vector<ObserverType*> new_observers_;
+  int deletion_count_ = 0;
+#ifndef NDEBUG
+  std::unique_ptr<ThreadChecker> thread_checker_;
+#endif
 };
 
 #define FOR_EACH_OBSERVER(ObserverType, observerList, func)   \
   do {                                                        \
+    observerList.VerifyThread();                              \
+    observerList.PrepareObserverListForIteration();           \
     for (ObserverType * observer : observerList.observers_) { \
-      observer->func;                                         \
+      if (observer) {                                         \
+        observer->func;                                       \
+      }                                                       \
     }                                                         \
   } while (0)
 
@@ -48,30 +111,74 @@ class ThreadSafeObserverList {
   // Add an observer to the list.  An observer should not be added to
   // the same list more than once.
   void AddObserver(ObserverType* observer) {
-    std::lock_guard<std::mutex> guard(list_mutex_);
-    observers_.push_back(observer);
+    std::lock_guard<std::mutex> guard(new_observers_mutex_);
+    new_observers_.push_back(observer);
   }
 
   // Remove an observer from the list if it is in the list.
   void RemoveObserver(ObserverType* observer) {
-    std::lock_guard<std::mutex> guard(list_mutex_);
-    for (ObserverType*& tempObserver : observers_) {
-      if (observer == tempObserver) {
-        tempObserver = observers_.back();
-        observers_.pop_back();
-        return;
-      }
-    }
-    assert(false);
+    std::lock_guard<std::mutex> guard(delete_observers_mutex_);
+    delete_observers_.push_back(observer);
   }
 
-  bool IsEmpty() { return observers_.empty(); }
+  void PrepareObserverListForIteration() {
+    {
+      std::lock_guard<std::mutex> guard(delete_observers_mutex_);
+      if (!delete_observers_.empty()) {
+        std::lock_guard<std::mutex> guard2(observers_mutex_);
+        for (ObserverType*& observer : observers_) {
+          for (auto* observer_to_delete : delete_observers_) {
+            if (observer == observer_to_delete) {
+              observer = observers_.back();
+              observers_.pop_back();
+            }
+          }
+        }
+        delete_observers_.clear();
+      }
+    }
+    {
+      std::lock_guard<std::mutex> guard(new_observers_mutex_);
+      if (!new_observers_.empty()) {
+        observers_.insert(std::end(observers_), std::begin(new_observers_),
+                          std::end(new_observers_));
+        new_observers_.clear();
+      }
+    }
+  }
 
   // A vector is used to keep the order of the observers deterministic.
   // Do not change to std::set.
   std::vector<ObserverType*> observers_;
-  std::mutex list_mutex_;
+  std::vector<ObserverType*> new_observers_;
+  std::vector<ObserverType*> delete_observers_;
+  std::mutex observers_mutex_;
+  std::mutex new_observers_mutex_;
+  std::mutex delete_observers_mutex_;
+  int deletion_count_ = 0;
 };
+
+// Thread safe version
+#define FOR_EACH_OBSERVER_TS(ObserverType, observerList, func)        \
+  do {                                                                \
+    observerList.PrepareObserverListForIteration();                   \
+    std::lock_guard<std::mutex> guard(observerList.observers_mutex_); \
+    for (ObserverType * observer : observerList.observers_) {         \
+      if (observer) {                                                 \
+        std::lock_guard<std::mutex> guard(                            \
+            observerList.delete_observers_mutex_);                    \
+        if (observerList.delete_observers_.size() > 0) {              \
+          for (ObserverType * observer_to_delete :                    \
+               observerList.delete_observers_) {                      \
+            if (observer == observer_to_delete) {                     \
+              continue;                                               \
+            }                                                         \
+          }                                                           \
+        }                                                             \
+        observer->func;                                               \
+      }                                                               \
+    }                                                                 \
+  } while (0)
 
 }  // namespace dux
 
--- test/test.cpp
+++ test/test.cpp
@@ -44,21 +44,33 @@ void TestMix() {
 class TestObserver {
  public:
   TestObserver() : value_(0) {}
-  void SetValue(int value) { value_ = value; }
+  virtual void SetValue(int value) { value_ = value; }
   int value_;
+};
+
+class TestObserverWithLambda : public TestObserver {
+ public:
+  TestObserverWithLambda(std::function<void()> lambda)
+      : lambda_(lambda), value_(0) {}
+  void SetValue(int value) override {
+    value_ = value;
+    lambda_();
+  }
+  int value_;
+  std::function<void()> lambda_;
 };
 
 void TestObserverList() {
   dux::ObserverList<TestObserver> observer_list;
   TestObserver observer_1;
   TestObserver observer_2;
-  assert(observer_list.IsEmpty());
+  assert(observer_list.ObserverCount() == 0);
 
   observer_list.AddObserver(&observer_1);
-  assert(!observer_list.IsEmpty());
+  assert(observer_list.ObserverCount() == 1);
 
   observer_list.AddObserver(&observer_2);
-  assert(!observer_list.IsEmpty());
+  assert(observer_list.ObserverCount() == 2);
 
   FOR_EACH_OBSERVER(TestObserver, observer_list, SetValue(10));
   assert(observer_1.value_ == 10);
@@ -68,14 +80,38 @@ void TestObserverList() {
   assert(observer_1.value_ == 100);
   assert(observer_2.value_ == 100);
 
+  assert(observer_list.ObserverCount() == 2);
   observer_list.RemoveObserver(&observer_1);
+  assert(observer_list.ObserverCount() == 1);
+
   FOR_EACH_OBSERVER(TestObserver, observer_list, SetValue(1000));
-  assert(!observer_list.IsEmpty());
   assert(observer_1.value_ == 100);
   assert(observer_2.value_ == 1000);
 
   observer_list.RemoveObserver(&observer_2);
-  assert(observer_list.IsEmpty());
+  assert(observer_list.ObserverCount() == 0);
+
+  // Test that modifying the observer list while iterating over it works.
+  TestObserver observer_3;
+  TestObserverWithLambda observer_4(
+      [&] { observer_list.RemoveObserver(&observer_3); });
+
+  assert(observer_3.value_ == 0);
+  assert(observer_4.value_ == 0);
+
+  observer_list.AddObserver(&observer_4);
+  observer_list.AddObserver(&observer_3);
+  observer_list.AddObserver(&observer_2);
+  observer_list.AddObserver(&observer_1);
+
+  assert(observer_list.ObserverCount() == 4);
+  FOR_EACH_OBSERVER(TestObserver, observer_list, SetValue(10000));
+  assert(observer_list.ObserverCount() == 3);
+
+  assert(observer_1.value_ == 10000);
+  assert(observer_2.value_ == 10000);
+  assert(observer_3.value_ == 0);
+  assert(observer_4.value_ == 10000);
 }
 
 void TestScopedLambda() {

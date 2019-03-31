512d40d21ebd6dc6b6df907e786f2d403703fefb
Jean-François Geyelin <jfgeyelin@gmail.com>
2019-03-31 22:51:58 +0200

    Added macros.

---
+++ src/macros.h
@@ -0,0 +1,15 @@
+#ifndef DUX_BASE_SRC_MACROS_H_
+#define DUX_BASE_SRC_MACROS_H_
+
+// Put this in the declarations for a class to be uncopyable.
+#define DISALLOW_COPY(TypeName) \
+  TypeName(const TypeName&) = delete
+// Put this in the declarations for a class to be unassignable.
+#define DISALLOW_ASSIGN(TypeName) TypeName& operator=(const TypeName&) = delete
+
+// Put this in the declarations for a class to be uncopyable and unassignable.
+#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
+  DISALLOW_COPY(TypeName);                 \
+  DISALLOW_ASSIGN(TypeName)
+
+#endif  // DUX_BASE_SRC_MACROS_H_

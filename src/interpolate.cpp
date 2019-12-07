d7721a395e669cc70f1eafce0863d017c50057f7
Jean-François Geyelin <jfgeyelin@gmail.com>
2019-12-07 22:42:12 +0100

    Remove poor interpolate functions.

--- src/interpolate.cpp
+++ src/interpolate.cpp
@@ -17,33 +17,22 @@ float Linear(float percentage) {
   return percentage;
 }
 
+//                   *
+//                  *
+//                **
+//           *****
+//  *********
 float QuadIn(float percentage) {
   return percentage * percentage;
 }
 
+//            *********
+//       *****
+//     **
+//    *
+//   *
 float QuadOut(float percentage) {
   return 1 - QuadIn(1 - percentage);
-}
-
-float ElasticIn(float percentage) {
-  const double pi(M_PI);
-  const double v(percentage - 1);
-  const double p(0.3);
-  return -std::pow(2, 10 * v) * std::sin((v - p / 4) * 2 * pi / p);
-  /*
-    float a = 10 * (percentage - 1);
-    float b = sin(((percentage - 1) - 0.3 / 4) * 2 * M_PI / 4);
-    float c = a * b;
-    float d = pow(2, c);
-    printf("%f %f %f %f\n", a,b,c,d);
-    return d;
-    */
-  // return pow(-2, 10 * (percentage - 1)) *
-  //        sin(((percentage - 1) - 0.3 / 4) * 2 * M_PI / 4);
-}
-
-float ElasticOut(float percentage) {
-  return 1 - ElasticIn(1 - percentage);
 }
 
 }  // namespace dux

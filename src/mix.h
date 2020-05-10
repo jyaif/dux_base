3a37cac9d444533fb6efbefbcc3f076651cbda86
Jean-François Geyelin <jfgeyelin@gmail.com>
2020-05-10 10:40:25 +0200

    Add nodiscard to dux::Mix. Fix Travis build.

--- src/mix.h
+++ src/mix.h
@@ -4,13 +4,13 @@
 namespace dux {
 
 template <class T, class S>
-T Mix(T const& a, T const& b, S progress) {
+[[nodiscard]] T Mix(T const& a, T const& b, S progress) {
   T diff = (b - a) * progress;
   return a + diff;
 }
 
 template <class T, class S>
-T Mix(T const& a, T const& b, S progress, T const& thresh_hold) {
+[[nodiscard]] T Mix(T const& a, T const& b, S progress, T const& thresh_hold) {
   T diff = (b - a);
   if (diff > thresh_hold || diff < -thresh_hold)
     return b;
